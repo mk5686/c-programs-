21c12eecd7e59b45e06cad78fe9cdc8a706b800c
mk5686 <komalmule2000@gmail.com>
2021-03-19 16:09:10 +0530

    simpla class programs

--- simple class/classexm.cpp
+++ simple class/classexm.cpp
@@ -22,12 +22,16 @@ int main(void)
 {
 	student s1;
 	student s2;
+	student s3;
 	s1.insert(101, "Komal");
 	s2.insert(102, " Swamini");
+	s3.insert(103, "Pratiksha");
 	
 	s1.display();
 	s2.display();
+	s3.display();
 	
+
 	
 	return 0;
 }
