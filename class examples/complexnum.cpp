c00c6ac2633e19f4b205725e1a34a9c1e1de4864
mk5686 <komalmule2000@gmail.com>
2021-03-20 14:30:00 +0530

    Object as a function argument

---
+++ class examples/complexnum.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+class complexNum
+	{
+	    int real,img ,sum ;
+	    public:
+	        void getData();
+	        void putData();
+	        void addition(complexNum,complexNum);
+	        void subtraction(complexNum, complexNum);
+
+	};
+	       
+	void complexNum :: getData()
+	{
+	    cout<<"\n Enter real and imaginary part of num \n in the form of n real + imaginary(i)\n";
+	    
+	    cin>>real>>img;
+	}
+	void complexNum :: putData()
+	{
+	    cout<<"\n "<<real<<"+"<<img<<"i"<<endl;
+	   
+	}
+	void complexNum :: addition (complexNum num1,complexNum num2)
+	{
+	            
+	    real = num1.real + num2.real ;
+	    img = num1.img + num2.img;
+	            
+	}
+	void complexNum :: subtraction (complexNum num1,complexNum num2)
+	{
+	            
+	    real = num1.real - num2.real ;
+	    img = num1.img - num2.img;
+	            
+	}
+	
+	
+
+int main() {
+	
+	complexNum ob1,ob2,s,a;
+	ob1.getData();
+	ob2.getData();
+	cout << "\nYou enterd numbers are :";
+	ob1.putData();
+	ob2.putData();
+
+	cout <<"\n Enter the choice:";
+	cout<<"\n 1.Addition ";
+	cout<<"\n 2.subtraction ";
+	int ch;
+	cin>>ch;
+
+	switch(ch)
+	{
+		case 1:a.addition(ob1,ob2);
+				cout << "\naddition is :";
+				a.putData();
+			break;
+		case 2:s.subtraction(ob1,ob2);
+				cout <<"\nsubtraction is :";
+				s.putData();
+			break;
+		default:cout<<"\n Invalid Input:";
+	}
+	/*a.addition(ob1,ob2);
+	s.subtraction(ob1,ob2);
+	
+	cout << "\naddition is :";
+	a.putData();*/
+	return 0;
+}
---
+++ class examples/shoppinglist.cpp
@@ -0,0 +1,97 @@
+#include<iostream>
+using namespace std;
+
+const int m = 50;
+
+class ITEMS
+{
+private:
+	int itemCode[m];
+	float itemPrice[m];
+	int count;
+public:
+	void CNT(void)
+	{
+		count=0;
+	}
+	void getitem(void);
+	void displaySum(void);
+	void remove(void);
+	void displayItems(void);
+};
+
+void ITEMS :: getitem(void)
+{
+	cout << "\n Enter item code :";
+	cin >> itemCode[count];
+
+	cout << "\n Enter item cost: ";
+	cin >> itemPrice[count];
+	count++;
+}
+void ITEMS :: displaySum(void)
+{
+	float sum = 0;
+	for (int i = 0; i < count; i++)
+	{
+		sum=sum+itemPrice[i];
+	}
+	cout << "\n Total value: "<< sum << endl;
+}
+void ITEMS :: remove(void)
+{
+	int a;
+	cout << "\n Enter the itam code: ";
+	cin >> a;
+	for(int i = 0; i < count ; i++)
+		if(itemCode[i]==a)
+			itemPrice[i] = 0;
+}
+void ITEMS :: displayItems()
+{
+	cout<<"\ncode price\n";
+	for ( int i = 0; i < count; i++)
+	{
+		cout << "\n" << itemCode[i];
+		cout << " " << itemPrice[i];
+	}
+	cout<<"\n";
+
+}
+
+int main()
+{
+	ITEMS order;
+	order .CNT();
+	int x;
+	do 
+	{
+		cout <<"\n You can do the following :";
+		cout <<"\n Enter the appropriate choice :";
+
+		cout<<" \n1 : Add an Item: ";
+		cout<<" \n2 : Display Total values:";
+		cout<<" \n3 : Delete and item";
+		cout<<" \n4 : Display aall iteams";
+		cout<<" \n5 : Quite";
+
+		cout<<"\n\n What is your choice: ";
+		cin>>x;
+		switch(x)
+		{
+			case 1:order.getitem();
+				break;
+			case 2:order.displaySum();
+				break;
+			case 3:order.remove();
+				break;
+			case 4:order.displayItems();
+				break;
+			case 5:break;
+			default: cout<<" \n Error in input ";
+
+		}
+	}while (x!=5);
+
+	return 0;
+}
